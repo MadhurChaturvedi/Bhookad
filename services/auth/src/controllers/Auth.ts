7632c8e4405d5bc7dde8d627987a5fd5c5ecbb6c
madhur <madhurchaturvedi2000@gmail.com>
2026-07-27 20:28:27 +0530

    add implemnt the TryCatch async middleware handler  + Test apis on post man

--- services/auth/src/controllers/Auth.ts
+++ services/auth/src/controllers/Auth.ts
@@ -1,31 +1,25 @@
-import { Request, Response } from "express";
 import User from "../model/User.js";
 import jwt from "jsonwebtoken";
+import TryCatch from "../middleware/tryCatch.js";
 
-export const loginUser = async (req: Request, res: Response) => {
-  try {
-    const { email, name, picture } = req.body;
-    let user = await User.findOne({ email });
-    if (!user) {
-      user = await new User({
-        email,
-        name,
-        image: picture,
-      });
-      user.save();
-    }
-    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
-      expiresIn: "15d",
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, name, picture } = req.body;
+  let user = await User.findOne({ email });
+  if (!user) {
+    user = await new User({
+      email,
+      name,
+      image: picture,
     });
-
-    res.status(200).json({
-      message: "Looged Success",
-      token,
-      user,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error,
-    });
+    user.save();
   }
-};
+  const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+    expiresIn: "15d",
+  });
+
+  res.status(200).json({
+    message: "Looged Success",
+    token,
+    user,
+  });
+});
